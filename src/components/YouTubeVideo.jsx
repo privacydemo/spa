92d0199dff4cb8405552d1a0e4ad5ada20d2afd1
privacydemo <aidan.morrissey@ankura.com>
2025-06-06 18:26:30 -0500

    yt

--- src/components/YouTubeVideo.jsx
+++ src/components/YouTubeVideo.jsx
@@ -2,17 +2,27 @@ import React from 'react';
 
 const YouTubeVideo = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem 0' }}>
-            <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/ugn_qmQ0NFo?si=3F3hUaouoOzUCp2e"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-            ></iframe>
+        <div id="youtubeVideoDiv" style={{ display: 'flex', justifyContent: 'center', margin: '2rem 0' }}>
+            <div class="gdpr-info-box js-data-gdpr-not-available-placeholder" style="display: none;">
+                <div id="ot-warning-text">
+                    <p class="title">We're sorry, this content is unavailable.</p>
+                    <p class="desc">To improve your experience, make sure Targeting cookies are enabled.</p>
+                </div>
+                <div id="ot-settings-btn">
+                    <a class="btn btn-tertiary" onclick="OneTrust.ToggleInfoDisplay()">Update your cookie settings</a>
+                </div>
+            </div>
+                <iframe
+                    className="optanon-category-C0004 embed-responsive-item"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/ugn_qmQ0NFo?si=3F3hUaouoOzUCp2e"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                ></iframe>
         </div>
     );
 };
